44fb88f1f757e3858fd5fdd4379564f7420c3e63
saharshbaiju <saharshbaiju@gmail.com>
2026-02-08 22:16:28 +0530

    corrected paths

--- api/login.ts
+++ api/login.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { getSheet } from './_sheet';
+import { getSheet } from './_sheet.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     if (req.method !== 'POST') {
